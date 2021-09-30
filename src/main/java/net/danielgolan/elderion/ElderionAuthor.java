f712a7f3aa6ccaf6c9fe117150263b9d48b4b67a
DanielGolan <paradisbloopi@gmail.com>
2021-10-01 02:00:42 +0300

    Added Voyager

--- src/main/java/net/danielgolan/elderion/ElderionAuthor.java
+++ src/main/java/net/danielgolan/elderion/ElderionAuthor.java
@@ -3,7 +3,7 @@ package net.danielgolan.elderion;
 import net.danielgolan.elderion.library.Author;
 
 public enum ElderionAuthor implements Author {
-    canedpeanutshels, daniel_golan, crystalline_robin;
+    canedpeanutshels, daniel_golan, crystalline_robin, voyager;
 
     @Override
     public String modID() {
